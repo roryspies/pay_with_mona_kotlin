531ed9fbbbfed7b36c3e6e43ee1c94fac65fa379
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2025-06-23 14:46:44 +0100

    Enhance Number formatting to adjust fraction digits based on value; set minimum fraction digits to 0 for whole numbers and 2 for decimals
    Signed-off-by: Jeffery Orazulike <chukwudumebiorazulike@gmail.com>

--- sdk/src/main/java/ng/mona/paywithmona/util/NumberExtensions.kt
+++ sdk/src/main/java/ng/mona/paywithmona/util/NumberExtensions.kt
@@ -4,5 +4,11 @@ import java.text.NumberFormat
 import java.util.Locale
 
 internal fun Number.format(): String {
-    return NumberFormat.getCurrencyInstance(Locale("en", "NG")).format(this.toFloat() / 100)
+    val corrected = this.toFloat() / 100
+    val formatter = NumberFormat.getCurrencyInstance(Locale("en", "NG"))
+
+    // Set minimum fraction digits to 0 for whole numbers, 2 for decimal values
+    formatter.minimumFractionDigits = if (corrected % 1 == 0f) 0 else 2
+
+    return formatter.format(corrected)
 }
